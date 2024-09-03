b9753ca79719bc220a2f1d868405ab60379bb582
Kerciu <Kerciuuu@gmail.com>
2024-09-03 12:40:57 +0200

    adjusted parameters of the ui once again, changed colors

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 460, 640);
+        Scene scene = new Scene(fxmlLoader.load(), 470, 640);
 
         URL cssUrl = getClass().getResource("/styles/styles.css");
         System.out.println("CSS URL: " + cssUrl);
