93acd18ccaaa5e5cd7a34efde7f77fc7e6d6679d
Kerciu <Kerciuuu@gmail.com>
2024-09-03 11:34:50 +0200

    successfully connected css file with fxml and main application

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -7,12 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 435, 560);
+
+        URL cssUrl = getClass().getResource("/styles/styles.css");
+        System.out.println("CSS URL: " + cssUrl);
+
         stage.setTitle("Easy Calc");
         StageIconLoader.loadIcon(this, stage);
         stage.setScene(scene);
