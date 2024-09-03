57d9b41fa803b100ca93ecc18c0c0bd0d958cee5
Kerciu <Kerciuuu@gmail.com>
2024-09-03 10:38:53 +0200

    added stage icon through stage icon loader class

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.frontend.StageIconLoader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,13 +12,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 435, 560);
+        stage.setTitle("Easy Calc");
+        StageIconLoader.loadIcon(this, stage);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
---
+++ src/main/java/com/example/frontend/StageIconLoader.java
@@ -0,0 +1,15 @@
+package com.example.frontend;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class StageIconLoader {
+    public static void loadIcon(Application source, Stage stage)
+    {
+        String iconPath = source.getClass().getResource("/assets/images/calculator.png").toString();
+        stage.getIcons().add(new Image(iconPath));
+    }
+}
